70d93f91acf38d257e7c4396073c2a3a2140e391
piru <121550328+erpirulin@users.noreply.github.com>
2024-08-14 18:47:08 +0200

    Porcetajes.c

---
+++ porcetajes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int number = 250;
+    int perc = 24;
+    
+    //completa el código
+    int result = number*perc/100;
+
+    printf("%d", result);
+
+    return 0;
+}
